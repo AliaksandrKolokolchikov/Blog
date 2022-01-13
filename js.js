9d31c989830b8f7c9bf46b704a2abfa541165720
гк <@adamant.local>
2022-01-13 21:12:16 +0300

    new

--- js.js
+++ js.js
@@ -1,11 +1,32 @@
-let a = 5;
-let b = 6;
-++a;
-const bigger = (number) => {
-  if (a == 5 || b == 6) {
-    console.log("Правда");
-  } else if (a == 7) {
-    console.log("Неправда");
+//let numberFirst = "5";
+//let numberSecond = 6;
+//let numberThree = 7;
+//let numberFour = 8;
+//let numberFive = 9;
+//++numberFirst;
+//const checkTypeOf = (argument) => {
+// if (typeof argument === "string") {
+//   console.log("yes");
+// } else if (typeof argument === "number") {
+//   console.log("no");
+// } else {
+//   console.log("smile");
+// }
+//};
+//checkTypeOf(numberFirst);
+
+let conter = 0;
+let maxNumber = 10;
+const counterLogger = (minConter, maxNumbers) => {
+  for (minConter = 0; minConter <= maxNumbers; minConter++) {
+    console.log(minConter);
+    if (minConter === 5) {
+      console.log("Середина");
+    }
+    if (minConter === maxNumbers) {
+      console.log("Конец");
+    }
   }
 };
-console.log(bigger());
+counterLogger(conter,maxNumber);
+┘^►kМ⌂.0nЬÒÓÔê|âÆ
