59b3566ebef645a9639d1c9c89914eb41e9438b9
гк <@adamant.local>
2022-01-17 21:08:31 +0300

    new

--- js.js
+++ js.js
@@ -15,18 +15,32 @@
 //};
 //checkTypeOf(numberFirst);
 
-let conter = 0;
-let maxNumber = 10;
-const counterLogger = (minConter, maxNumbers) => {
-  for (minConter = 0; minConter <= maxNumbers; minConter++) {
-    console.log(minConter);
-    if (minConter === 5) {
-      console.log("Середина");
-    }
-    if (minConter === maxNumbers) {
-      console.log("Конец");
-    }
+//let conter = 0;
+//let maxNumber = 10;
+//const counterLogger = (minConter, maxNumbers) => {
+//  for (minConter = 0; minConter <= maxNumbers; minConter++) {
+  //  console.log(minConter);
+ //   if (minConter === 5) {
+  //    console.log("Середина");
+  //  }
+  //  if (minConter === maxNumbers) {
+  //    console.log("Конец");
+  //  }
+ // }
+//};
+//counterLogger(conter,maxNumber);
+
+let string = "hello"
+const arr = string.split("");
+const newString = (arg) => {
+  const newARr = [];
+  for (let i = 0; i <= arg.lenght; i++) {
+    if (arg[i] === "l") {
+    newARr.push("h");
+    }else newARr.push(arg[i]);
   }
+  return newARr.join("");
 };
-counterLogger(conter,maxNumber);
-┘^►kМ⌂.0nЬÒÓÔê|âÆ
+
+
+
